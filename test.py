c4dfc6a0c618b2ce6a37815b9d7140e6fe35135a
Robin Winslow <robin@robinwinslow.co.uk>
2022-08-11 14:23:17 +0100

    Also test accented characters

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from tweet_counter import count_tweet
 tweet_text = (
     "Otters 👪 otters 𝗼𝘁𝘁𝗲𝗿𝘀 otters otters.otters otters.com/otters otters "
     "otters http://otters otters https://otters.uk otters otters.com otters "
-    "otters otters otters otters otters otters otters otters otters otters "
+    "otters otters otters ótters Ótters òttérs otters otters otters otters "
     "otters.​biz otters otters otters otter"
 )
 
